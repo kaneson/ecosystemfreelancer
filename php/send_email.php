b212fbc058b98dce0fd955b6a8353edc74ede006
Kaneson Alves <kanesonalves@pop-os.localdomain>
2021-04-21 22:47:15 +0100

    add button form and change and dynamic get post dataw

--- php/send_email.php
+++ php/send_email.php
@@ -1,8 +1,14 @@
 <?php
+
 $your_email="info@ecosystemfreelancer.com";
 
-if(!empty($_POST))
-{
+if(!empty($_POST)) {
+	
+	foreach ($_POST as $field => $value) {
+		$$field = $value;
+		$data[$field] = $value;
+	}
+
 	$name=$_POST['name'];
 	$email=$_POST['email'];
 	$message=$_POST['message'];
@@ -14,6 +20,7 @@ if(!empty($_POST))
 	$message = $name.' sent you a message via the contact form :'."\r\n".$message;
 	
 	mail($to, $subject, $message, $headers);
+
 }
 
 ?>
